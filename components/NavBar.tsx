b2f299f828a7181d7f72fe88fd61c31dc55c4eed
Lobbbey <joshuatylerbandy@gmail.com>
2025-03-14 00:30:22 -0400

    no idea what I'm doing

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,55 +1,18 @@
-import{Link, useMatch, useResolvedPath, NavLink} from "react-router-dom";
-import { ReactNode } from "react";
-import { FaGithub, FaLinkedin, FaFileAlt } from 'react-icons/fa';
-import Pdf from "/public/resume.pdf";
-//import './NavBar.css'; 
+'use client';
+
+import Link from 'next/Link';
+import { useState } from 'react';
+import {Sheet, SheetContent, SheetHeader, SheetTrigger } from "./ui/sheet"
+
+
 
 export default function NavBar(){
+
     return(
-    <nav className="nav">
-        <Link to='/' className='siteTitle'>Joshua Bandy</Link>
-        <ul>
-            <CustomLink to="/">Home</CustomLink>
-            <CustomLink to="/projects">Projects</CustomLink>
-            <CustomLink to="/experience">Experience</CustomLink>
-            <CustomLink to="/skills">Skills</CustomLink>
-            <li><a href={Pdf} target="_blank" rel="noopener noreferrer"><FaFileAlt size={20}/></a></li>
-            <li><a href="https://github.com/Lobbbey" target="_blank" rel="noopener noreferrer"><FaGithub size={20}/></a></li>
-            <li><a href="https://www.linkedin.com/in/joshua-tyler-bandy/" target="_blank" rel="noopener noreferrer"><FaLinkedin size={20}/></a></li>
-        </ul>
-    </nav>
-    );
-}
+        <>
+            <nav>
 
-interface CustomLinkProps {
-    to: string;
-    children: ReactNode;
-    [key: string]: any; // to allow any other props like className, etc.
-}
-
-
-/* function CustomLink({ to, children, ...props}: CustomLinkProps){
-    const resolvedPath = useResolvedPath(to);
-    const isActive = useMatch({path: resolvedPath.pathname, end: true});
-    return(
-        <li className={isActive ? "active" : ""}>
-            <Link to={to} {...props}>
-                {children}
-            </Link>
-        </li>
-    );
-} */
-
-function CustomLink({ to, children, ...props }: CustomLinkProps) {
-    return (
-        <li><NavLink
-                to={to}
-                {...props}
-                className={({ isActive }) =>
-                    isActive ? "active underline font-bold" : "hover:underline"
-                }>
-                    {children}
-            </NavLink>
-        </li>
+            </nav>
+        </>
     );
 }
