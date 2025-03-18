bb9a27b28cf2a1def4cd4c04c6a7c2d16c824261
Lobbbey <joshuatylerbandy@gmail.com>
2025-03-17 22:38:32 -0400

    idk some edits ig

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,17 +1,34 @@
 'use client';
 
-import Link from 'next/Link';
+import Link from 'next/link';
 import { useState } from 'react';
-import {Sheet, SheetContent, SheetHeader, SheetTrigger } from "./ui/sheet"
-
-
 
 export default function NavBar(){
+    const [prevScrollPos, setPrevScrollPos] = useState(0);
+    const [visible, setVisibile] = useState(0);
 
     return(
         <>
-            <nav>
-
+            <nav className='Fixed w-full h-10 shadow-xl bg-[#28262C]'>
+                <div className='flex justify-between items-center px-3  text-[#FCF7F8] h-full w-full'>
+                    <Link href="/">Joshua Bandy</Link>
+                    <div>
+                        <ul className='hidden sm:flex'>
+                            <Link href="/">
+                                <li>Home</li>
+                            </Link>
+                            <Link href="/Projects">
+                                <li>Projects</li>
+                            </Link>
+                            <Link href="/Education">
+                                <li>Education</li>
+                            </Link>
+                            <Link href="/Skills">
+                                <li>Skills</li>
+                            </Link>
+                        </ul>
+                    </div>
+                </div>
             </nav>
         </>
     );
