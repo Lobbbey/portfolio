94415670530268dfeb7b1be59c8731cf8ce3d4df
Lobbbey <joshuatylerbandy@gmail.com>
2025-05-22 12:03:12 -0400

    updated resume

--- app/page.tsx
+++ app/page.tsx
@@ -2,17 +2,19 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <section className="flex items-center justify-center h-full px-2 md:px-8">
+    <section className="flex items-center mt-5 mx-5">
+      
       {/*Page Title*/}
-      <div> 
-        <h1 className="text-2xl">Welcome, I&apos;m Joshua!</h1>
+      <div className="text-3xl "> 
+        <h1 className="">Welcome, I&apos;m Joshua!</h1>
       </div>
       
       {/*About me Paragraph*/}
-      <div className="pt-20 space-y-4 md:pb-0 pb-12">
-        <p className="text-lg">
+      <div className="">
+        <p className="">
           I am a Software Engineer and first generation undergraduate
-          senior at the{" "}<Link href={"https://ucf.edu/"} className="underline">Univeristy of Central Florida</Link>{" "}
+          senior at the{" "}<Link href={"https://ucf.edu/"} target="_blank" className="underline">Univeristy of Central Florida</Link>.<br/>
+          
         </p>
       </div>
 
--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -8,13 +8,13 @@ export default function NavBar(){
 
     return(
         <nav className='Fixed w-full h-10 shadow-xl bg-[#1B2021]'>
-            <div className='flex justify-between items-center px-4  text-[#FCF7F8] h-full w-full'>
+            <div className='flex justify-between items-center px-8  text-[#FCF7F8] h-full w-full'>
                 <Link href="/">
                     <span className='hover:border-b text-xl'>Joshua Bandy</span>
                 </Link>
                 <div>
                     <ul className='hidden sm:flex items-center space-x-10'>
-                        <Link href="/"><li className='hover:border-b text-lg'>Home</li></Link>
+                        {/*<Link href="/"><li className='hover:border-b text-lg'>Home</li></Link>*/}
                         <Link href="/projects"><li className='hover:border-b text-lg'>Projects</li></Link>
                         <Link href="/experience"><li className='hover:border-b text-lg'>Experience</li></Link>
                         <Link href="/skills"><li className='hover:border-b text-lg'>Skills</li></Link>
