3dad73460c4670d4efbaf5a2d5ac740408cf52d3
Lobbbey <joshuatylerbandy@gmail.com>
2025-05-25 12:19:32 -0400

    see more section and navbar thickness

--- app/aboutme/page.tsx
+++ app/aboutme/page.tsx
@@ -1,32 +1,35 @@
-import { FaGithub, FaLinkedin } from 'react-icons/fa';
-import { FaFileLines } from 'react-icons/fa6'
 import Link from 'next/link';
 
 export default function Experience() {
     return (
-        <section className="flex flex-col justify-center items-center pt-10">
+        <section className="my-5  md:p-5" id='about'>
 
-            {/*Page title*/}
-            <div className="text-5xl">
+            {/*About me*/}
+            <div className="text-5xl sm:text-4xl text-center">
                 <h1>About Me</h1>
             </div>
-
-            {/*About Me*/}
 
             {/*Skills*/}
 
             {/*See More Links*/}
-            <div>
-                <h2 className="text-2xl">See more here!</h2>
-                    <Link href="/resume.pdf" target='_blank' className='hover:border-b'>
-                        <FaFileLines className='text-2xl' title='Resume' />Resume
+            <div className='mt-14 flex flex-col items-center text-center'>
+                <h2 className="text-2xl md:text-3xl text-nowrap mt-8 mb-6">See more here!</h2>
+                <div className="flex gap-16 mt-4 md:mt-8">
+                    <Link href="/resume.pdf" target='_blank' className='transition-all duration-300 hover:text-[#eb9c35] '>
+                        <img src="/icons/resume.svg" alt="Resume" className="w-12 h-12 transform duration-300 hover:scale-110"/>
+                        <span className='text-center text-sm mt-2'>Resume</span>
                     </Link>
-                    <Link href="https://github.com/Lobbbey" target="_blank" className='hover:border-b'>
-                        <FaGithub className='text-lg' title='Github' />
+
+                    <Link href="https://github.com/Lobbbey" target="_blank" className='transition-all duration-300 hover:text-[#eb9c35]'>
+                        <img src='/icons/github.svg' alt="GitHub" className='w-12 h-12 transform duration-300 hover:scale-110'/>
+                        <span className='text-center text-sm mt-2'>GitHub</span>
                     </Link>
-                    <Link href="https://www.linkedin.com/in/joshua-tyler-bandy/" target="_blank" className='hover:border-b'>
-                        <FaLinkedin className='text-lg' title='LinkedIn' />
+ 
+                    <Link href="https://www.linkedin.com/in/joshua-tyler-bandy/" target="_blank" className='transition-all duration-300 hover:text-[#eb9c35] '>
+                        <img src='/icons/linkedin.svg' alt="LinkedIn" className="w-12 h-12 transform duration-300 hover:scale-110 "/>
+                        <span className='text-center text-sm mt-2'>LinkedIn</span>
                     </Link>
+                </div>
             </div>
         </section>
     );
--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,13 +1,12 @@
 'use client';
 import Link from 'next/link';
-import { useState } from 'react';
 import { FaGithub, FaLinkedin} from 'react-icons/fa';
 import {FaFileLines } from 'react-icons/fa6'
 
 export default function NavBar(){
 
     return(
-        <nav className='Fixed w-full h-10 shadow-xl bg-[#1B2021]'>
+        <nav className='Fixed w-full h-15 shadow-xl bg-[#1B2021]'>
             <div className='flex justify-between items-center px-8 text-[#ede0d4] h-full w-full'>
                 <Link href="/">
                     <span className='hover:border-b transition-all duration-300 hover:text-[#c4501b] text-xl'>Joshua Bandy</span>
@@ -17,13 +16,13 @@ export default function NavBar(){
                         <Link href="/"><li className='hover:border-b transition-all duration-300 text-lg hover:text-[#c4501b]'>Home</li></Link>
                         <Link href="/aboutme"><li className='hover:border-b text-lg transition-all duration-300 hover:text-[#c4501b]'>About Me</li></Link>
                         <Link href="/projects"><li className='hover:border-b text-lg  transition-all duration-300 hover:text-[#c4501b]'>Projects</li></Link>
-                        <Link href="/resume.pdf" target='_blank' className='hover:border-b transition-all duration-300 hover:text-[#c4501b]'>
+                        <Link href="/resume.pdf" target='_blank' className='transition-all duration-300 hover:text-[#c4501b]'>
                             <FaFileLines className='text-lg transition-all duration-300 hover:text-[#c4501b]' title='Resume'/>
                         </Link>
-                        <Link href="https://github.com/Lobbbey" target="_blank" className='hover:border-b transition-all duration-300 hover:text-[#c4501b]'>
+                        <Link href="https://github.com/Lobbbey" target="_blank" className='transition-all duration-300 hover:text-[#c4501b]'>
                             <FaGithub className='text-lg' title='Github'/>
                         </Link>
-                        <Link href="https://www.linkedin.com/in/joshua-tyler-bandy/" target="_blank" className='hover:border-b'>
+                        <Link href="https://www.linkedin.com/in/joshua-tyler-bandy/" target="_blank" className=''>
                             <FaLinkedin className='text-lg transition-all duration-300 hover:text-[#c4501b]' title='LinkedIn'/>
                         </Link>
                     </ul>
