bb78c125272b3b2d7d20a03b2b492e2e480d746c
Lobbbey <joshuatylerbandy@gmail.com>
2025-05-30 22:42:09 -0400

    removed cards

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,9 +1,3 @@
-import {
-    Card,
-    CardContent,
-    CardHeader,
-    CardFooter,
-} from "@/components/ui/card";
 import { FaParagraph } from "react-icons/fa";
 
 export default function Projects() {
